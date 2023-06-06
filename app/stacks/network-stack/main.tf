fa038de5dbfee92ff87ec685bc7bb4ea92383c5f
clxdev <domhenin.517@gmail.com>
2023-06-06 11:10:51 -0400

    commit

--- app/stacks/network-stack/main.tf
+++ app/stacks/network-stack/main.tf
@@ -6,30 +6,30 @@
 module "network-base" {
   source = "../../modules/network-base"
 
-#-- vpc  
+  #-- vpc  
   vpc_cidr = var.vpc_cidr
   vpc_tags = var.vpc_tags
 
-#-- public subnet
+  #-- public subnet
   pub_sub_cidr = var.pub_sub_cidr
   pub_sub_tags = var.pub_sub_tags
 
-#-- private subnet
+  #-- private subnet
   priv_sub_cidr = var.priv_sub_cidr
   priv_sub_tags = var.priv_sub_tags
 
-#-- internet gateway
+  #-- internet gateway
   wl_igw_tags = var.wl_igw_tags
 
-#-- route table:- public
-  rt_pub_tag     = var.rt_pub_tag 
+  #-- route table:- public
+  rt_pub_tag = var.rt_pub_tag
   # rt_pub_asso_tag = var.rt_pub_asso_tag
 
-#-- security group:- public
+  #-- security group:- public
   sg_ajar_name = var.sg_ajar_name
   ajar_sg_tag  = var.ajar_sg_tag
 
-#-- security group:- private
+  #-- security group:- private
   sg_priv_name = var.sg_priv_name
   priv_sg_tag  = var.priv_sg_tag
 }
